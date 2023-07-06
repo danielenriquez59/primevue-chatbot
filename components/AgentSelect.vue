79e7719575a2613682010e91221b7058efff3d01
danielenriquez59 <deenriquez92@gmail.com>
2023-07-06 13:15:40 -0700

    minor fix to prevent warnings on load

--- components/AgentSelect.vue
+++ components/AgentSelect.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex w-1/4 items-center space-x-4">
         <span>Agent:</span>
-        <Dropdown v-model="currentAgent" :options="options" optionLabel="name" placeholder="Select an Agent"
+        <Dropdown v-model="currentAgent" :options="options" optionLabel="name" placeholder="Agent Default"
             class="w-full md:w-14rem" />
     </div>
 </template>
